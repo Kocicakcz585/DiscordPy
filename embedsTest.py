aeaf6e60c6841b4320a687312bda5291526d3b5f
Kocicakcz585 <kocicakcz585@gmail.com>
2025-05-23 15:57:28 +0200

    finalized embedsTest.py, made it work

--- embedsTest.py
+++ embedsTest.py
@@ -3,6 +3,7 @@ import shutil
 import discord
 import json
 from discord import app_commands
+from discord.ext import commands
 import logging
 import os
 from dotenv import load_dotenv
@@ -19,7 +20,7 @@ templateUserData = 'template.json'
 userDataLocation = os.getenv('USER_DATA_LOC')
 
 class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
+    def __init__(self, *, intents: discord.Intents) -> None:
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
@@ -27,9 +28,59 @@ class MyClient(discord.Client):
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
+class ClickTheCookieButton(discord.ui.View):
+    # def __init__(self):
+    #     super().__init__()
+    #     self.value = None
+
+    @discord.ui.button(label='Click 🍪', style=discord.ButtonStyle.gray)
+    async def Click(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        user = interaction.user
+        clickCookie(user)
+        data = getUserData(user)
+        cookies = getCookiesFromData(data)
+        embed = discord.Embed(
+        title=None,
+            description=None,
+            color=discord.Color.dark_orange(),
+            timestamp=datetime.datetime.now()
+        )
+
+        view = ClickTheCookieButton()
+
+        embed.set_author(name='Cookie Clicker bot 🍪')
+        embed.add_field(name='Cookie Count', value=f'{user.global_name} has {cookies} cookies 🍪')
+        await interaction.response.edit_message(embed=embed, view=view)
+        self.stop()
+
 client = MyClient(intents=intents)
 
-def getUserData(user):
+def clickCookie(user) -> None:
+    file = userDataLocation + str(user.id) + ".json"
+
+    if not os.path.exists(file):
+        getUserData(user)
+
+    with open(file, 'r+') as f:
+            fileData = json.load(f)
+
+            CookiesPerClick = fileData['upgrades']['MoreCookiesPerClick']
+            CookiesPerClick = int(CookiesPerClick)
+            CookiesPerClick += 1
+
+            cookies = fileData['cookies']
+            cookies = int(cookies)
+            cookies += CookiesPerClick
+            
+
+            fileData['cookies'] = str(cookies)
+
+            f.seek(0)
+            print(f'f: {f}, \n FileData: {fileData}')
+            json.dump(fileData, f)
+            f.truncate()
+
+def getUserData(user):# -> Any:
     userID = user.id
     file = userDataLocation + str(userID) + ".json"
 
@@ -52,7 +103,7 @@ def getUserData(user):
 
     return data
 
-def getCookiesFromData(data):
+def getCookiesFromData(data):# -> Any:
     cookies = data['cookies']
     return cookies
 
@@ -73,8 +124,14 @@ async def testFunc(Interaction: discord.Interaction) -> None:
         timestamp=datetime.datetime.now()
     )
 
-    embed.set_author(name='Cookie Clicker bot 🍪')
-    embed.add_field(name='Cookie Count', value=f'{cookies} cookies 🍪')
-    await Interaction.response.send_message(embed=embed)
+    view = ClickTheCookieButton()
 
-client.run(token=token, log_handler=handler, log_level=logging.DEBUG)
+    embed.set_author(name='Cookie Clicker bot 🍪')
+    embed.add_field(name='Cookie Count', value=f'{user.global_name} has {cookies} cookies 🍪')
+    await Interaction.response.send_message(embed=embed, view=view)
+
+
+
+
+
+client.run(token=token, log_handler=handler, log_level=logging.INFO)
